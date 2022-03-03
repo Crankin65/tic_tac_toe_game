46bd738552243b9d38ff304ef921f732162767f1
Cameron Rankin <crankin65@gmail.com>
2022-03-03 12:00:58 -0500

    Added Player class, Consolidated game_turn, Started building out game win conditions

--- Tic_tac_toe.rb
+++ Tic_tac_toe.rb
@@ -1,3 +1,6 @@
+# 3/3/22 Update columns/diagonals in board 
+# 
+
 =begin Improvements to make:
 - Each turn doesn't need to be a method.
 - Add error message if you don't take your turn or if you put in a number for a square that's already been entered or more than 9
@@ -10,31 +13,7 @@
 =end
  
 # Class to collect each player's name, symbol, and turn order
-class Player 
-  attr_accessor :name
-  attr_accessor :symbol
-  attr_accessor :took_first_turn
-  
-  def initialize(name,symbol) 
-    @name = name
-    @symbol = symbol
-    $took_first_turn = took_first_turn
-    #@player_id += 1
-  end
 
-  def set_name=(name)
-    @name = name
-  end
-
-  def set_symbol=(symbol)
-    @symbol = symbol
-  end
-
-  def first_turn=(took_first_turn)
-    @took_first_turn = took_first_turn
-  end
-
-end
 
 # Board display via array
 def display_board(board)
@@ -46,6 +25,41 @@ def display_board(board)
 end
 
 $board = ["1","2","3","4","5","6","7","8","9"]
+
+$board2 = ["X","X","X","4","5","6","7","8","9"]
+# 3 in a row, but only starting at 0,4,7
+
+$board3 = ["X","2","3","X","5","6","X","8","9"]
+# Any 3 , only start 0 (+3, +3), 1, 2
+
+$board4 = ["X","2","3","4","X","6","7","8","X"]
+# Any 3, only starting, 0 or 2, 4 has to (center), 6(2) or 8 (0)
+# 0,4,8
+# 2,4,6
+
+def rows
+  [
+    [$board[0], $board[1], $board[2]],
+    [$board[3], $board[4], $board[5]],
+    [$board[6], $board[7], $board[8]]
+  ]
+end
+
+def column
+  [
+  [$board[0], $board[3], $board[6]],
+  [$board[1], $board[4], $board[7]],
+  [$board[2], $board[5], $board[8]]
+  ]
+end
+
+def diagonal
+  [
+    [$board[0], $board[4], $board[8]],
+    [$board[2], $board[4], $board[6]],
+  ]
+end
+
 
 # Lists winning conditions possible
 def game_condition()
@@ -138,267 +152,61 @@ def game_condition()
   end
 end
 
-# Changes board array to symbols
-def game_enter(symbol, square)
-  case tac_toe
-  when square = i, 
-    board[i]= symbol
+
+class Player
+  attr_reader :name, :symbol
+
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
   end
-  puts board
 end
 
 #Prompt for Player One to update player class
 puts "Welcome to the game of tic-tac-toe! Player one, what is your name?"
 
-player_one = Player.new(" "," ")
-player_two = Player.new(" "," ")
+$update_player_one_name = gets.chomp
 
-player_one.set_name = gets.chomp
-$update_player_one_name = player_one.name.delete("\"")
+puts "What symbol will you use?" 
+$update_player_one_symbol = gets.chomp
 
-puts "What symbol will you use?"
-player_one.set_symbol = gets.chomp
-$update_player_one_symbol = player_one.symbol.delete("\"")
+
 
 puts "You'll be playing as #{$update_player_one_name} and using the #{$update_player_one_symbol}!"
 
 #Prompt for Player Two to update player class
 puts "Player two, what is your name?"
-
-player_two.set_name = gets.chomp
-$update_player_two_name = player_two.name.delete("\"")
+$update_player_two_name = gets.chomp
 
 puts "What symbol will you use?"
-player_two.set_symbol = gets.chomp
-$update_player_two_symbol = player_two.symbol.delete("\"")
+$update_player_two_symbol = gets.chomp
 
 puts "You'll be playing as #{$update_player_two_name} and using the #{$update_player_two_symbol}!"
 
 #Game Starts
 puts "Now let the game begin!"
 
-display_board($board)
-
-#Randomly chooses turn order
-def random_start()
-  if rand(1..2) == 1
-    puts "#{$update_player_one_name} starts! What square would you like?" 
-    player_one_first = gets.chomp
-    update_player_one_first = (player_one_first.to_i - 1)
-    $board[update_player_one_first] = $update_player_one_symbol
-  else rand(1..2) > 2
-    puts "#{$update_player_two_name} starts! What square would you like?" 
-    player_two_first = gets.chomp
-    update_player_two_first = (player_two_first.to_i - 1)
-    $board[update_player_two_first] = $update_player_two_symbol
-  end
-end
-
-random_start()
+$player1 = Player.new($update_player_one_name, $update_player_one_symbol)
+$player2 = Player.new($update_player_two_name, $update_player_two_symbol)
+$current_player = $player1
 
 display_board($board)
-
-def check_on_first_turn()
-  if $board.any? {|symbol| symbol == $update_player_one_symbol} 
-    $player_one_took_first_turn = true
-    #puts  "$player_one_took_first_turn" 
-  elsif $board.any? {|symbol| symbol == $update_player_two_symbol} 
-    $player_two_took_first_turn = true
-    #puts "$player_two_took_first_turn"
-  else puts  "WTF, check first turn didn't work" 
-  end
-end
-   
-check_on_first_turn()
-
-# def next_turn()
-#   if $player_one_took_first_turn == 1
-#     puts "#{$update_player_two_name} what is your move?"
-#     player_two_second = gets.chomp
-#     update_player_two_second = (player_two_second.to_i - 1)
-#     $board[update_player_two_second] = $update_player_two_symbol
-#     display_board($board)
-#   elsif $player_two_took_first_turn == 1
-#     puts "#{$update_player_one_name} what is your move?"
-#     player_first_second = gets.chomp
-#     update_player_first_second = (player_first_second.to_i - 1)
-#     $board[update_player_first_second] = $update_player_one_symbol
-#     display_board($board)
-#   else puts "WTF? next turn didn't work"
-#   end
-# end
-
-# next_turn()
-
-# def third_turn()
-#   if $player_one_took_first_turn == 1
-#     puts "#{$update_player_one_name} what is your move?"
-#     player_one_third = gets.chomp
-#     update_player_one_third = (player_one_third.to_i - 1)
-#     $board[update_player_one_third] = $update_player_one_symbol
-#     display_board($board)
-#   elsif $player_two_took_first_turn == 1
-#     puts "#{$update_player_two_name} what is your move?"
-#     player_two_third = gets.chomp
-#     update_player_two_third = (player_two_third.to_i - 1)
-#     $board[update_player_two_third] = $update_player_two_symbol
-#     display_board($board)
-#   else puts "WTF? third turn didn't work"
-#   end
-# end
-
-# third_turn()
-
-# def fourth_turn()
-#   if $player_one_took_first_turn == 1
-#     puts "#{$update_player_two_name} what is your move?"
-#     player_two_fourth = gets.chomp
-#     update_player_two_fourth = (player_two_fourth.to_i - 1)
-#     $board[update_player_two_fourth] = $update_player_two_symbol
-#     display_board($board)
-#   elsif $player_two_took_first_turn == 1
-#     puts "#{$update_player_one_name} what is your move?"
-#     player_one_fourth = gets.chomp
-#     update_player_one_fourth = (player_one_fourth.to_i - 1)
-#     $board[update_player_one_fourth] = $update_player_one_symbol
-#     display_board($board)
-#   else puts "WTF? fourth turn didn't work"
-#   end
-# end
-
-# fourth_turn()
-
-# game_condition()
-
-# def fifth_turn()
-#   if ($player_one_took_first_turn == 1) and ($condition != (1 or 2))
-#     puts "#{$update_player_one_name} what is your move?"
-#     player_one_fifth = gets.chomp
-#     update_player_one_fifth = (player_one_fifth.to_i - 1)
-#     $board[update_player_one_fifth] = $update_player_one_symbol
-#     display_board($board)
-#   elsif ($player_two_took_first_turn == 1) and ($condition != (1 or 2))
-#     puts "#{$update_player_two_name} what is your move?"
-#     player_two_fifth = gets.chomp
-#     update_player_two_fifth = (player_two_fifth.to_i - 1)
-#     $board[update_player_two_fifth] = $update_player_two_symbol
-#     display_board($board)
-#   else
-#   end
-# end
-
-# fifth_turn()
-
-# game_condition()
-
-# def sixth_turn()
-#   if $player_one_took_first_turn == 1 and ($condition != (1 or 2))
-#     puts "#{$update_player_two_name} what is your move?"
-#     player_two_sixth = gets.chomp
-#     update_player_two_sixth = (player_two_sixth.to_i - 1)
-#     $board[update_player_two_sixth] = $update_player_two_symbol
-#     display_board($board)
-#   elsif $player_two_took_first_turn == 1 and ($condition != (1 or 2))
-#     puts "#{$update_player_one_name} what is your move?"
-#     player_one_sixth = gets.chomp
-#     update_player_one_sixth = (player_one_sixth.to_i - 1)
-#     $board[update_player_one_sixth] = $update_player_one_symbol
-#     display_board($board)
-#   else
-#   end
-# end
-
-# sixth_turn()
-
-# game_condition()
-
-# def seventh_turn()
-#   if $player_one_took_first_turn == 1 and ($condition != (1 or 2))
-#     puts "#{$update_player_one_name} what is your move?"
-#     player_one_seventh = gets.chomp
-#     update_player_one_seventh = (player_one_seventh.to_i - 1)
-#     $board[update_player_one_seventh] = $update_player_one_symbol
-#     display_board($board)
-#   elsif $player_two_took_first_turn == 1 and ($condition != (1 or 2))
-#     puts "#{$update_player_two_name} what is your move?"
-#     player_two_seventh = gets.chomp
-#     update_player_two_seventh = (player_two_seventh.to_i - 1)
-#     $board[update_player_two_seventh] = $update_player_two_symbol
-#     display_board($board)
-#   else 
-#   end
-# end
-
-# seventh_turn()
-
-# game_condition()
-
-# def eigth_turn()
-#   if $player_one_took_first_turn == 1 and ($condition != (1 or 2))
-#     puts "#{$update_player_two_name} what is your move?"
-#     player_two_eigth = gets.chomp
-#     update_player_two_eigth = (player_two_eigth.to_i - 1)
-#     $board[update_player_two_eigth] = $update_player_two_symbol
-#     display_board($board)
-#   elsif $player_two_took_first_turn == 1 and ($condition != (1 or 2))
-#     puts "#{$update_player_one_name} what is your move?"
-#     player_one_eigth = gets.chomp
-#     update_player_one_eigth = (player_one_eigth.to_i - 1)
-#     $board[update_player_one_eigth] = $update_player_one_symbol
-#     display_board($board)
-#   else 
-#   end
-# end
-
-# eigth_turn()
-
-# game_condition()
-
-# def ninth_turn()
-#   if $player_one_took_first_turn == 1 and ($condition != (1 or 2))
-#     puts "#{$update_player_one_name} what is your move?"
-#     player_one_ninth = gets.chomp
-#     update_player_one_ninth = (player_one_ninth.to_i - 1)
-#     $board[update_player_one_ninth] = $update_player_one_symbol
-#     display_board($board)
-#   elsif $player_two_took_first_turn == 1 and ($condition != (1 or 2))
-#     puts "#{$update_player_two_name} what is your move?"
-#     player_two_ninth = gets.chomp
-#     update_player_two_ninth = (player_two_ninth.to_i - 1)
-#     $board[update_player_two_ninth] = $update_player_two_symbol
-#     display_board($board)
-#   else 
-#   end
-# end
-
-# ninth_turn()
-
-# game_condition()
 
 def game_over?
   $condition == 1 || $condition == 2
 end
 
 def game_turn
-  player_name = nil
-  symbol = nil
-  if $player_two_took_first_turn == 1 && !game_over?
-    player_name = $update_player_one_name
-    symbol = $update_player_one_symbol
-  else
-    player_name = $update_player_two_name
-    symbol = $update_player_two_symbol
+  if !game_over? 
+    puts "#{$current_player.name} what is your move?"
+    response = gets.chomp.to_i - 1
+    $board[response] = $current_player.symbol
+    $current_player = $current_player == $player1 ? $player2 : $player1
   end
-
-  puts "#{player_name} what is your move?"
-  response = gets.chomp.to_i - 1
-  $board[response] = symbol
-  $player_one_took_first_turn = !$player_one_took_first_turn
-  $player_two_took_first_turn = !$player_two_took_first_turn
 end
 
 
-8.times do
+9.times do
   game_turn
   display_board($board)
   game_condition
